aeeb0c4b598042cef2b5f9ef89308825682ca5c3
Ioannis Mer <ioannismeri@gmail.com>
2018-02-26 06:46:58 +0000

    remove captcha temporarily

--- routes/index.js
+++ routes/index.js
@@ -55,23 +55,23 @@ router.get("/register", function(req, res) {
 
 //HANDLE SIGN UP LOGIC
 router.post("/register", function(req, res){
-  const captcha = req.body["g-recaptcha-response"];
-  if (!captcha) {
-    console.log(req.body);
-    req.flash("error", "Please select captcha");
-    return res.redirect("/register");
+  // const captcha = req.body["g-recaptcha-response"];
+  // if (!captcha) {
+  //   console.log(req.body);
+  //   req.flash("error", "Please select captcha");
+  //   return res.redirect("/register");
   }
-    // secret key
-    var secretKey = process.env.CAPTCHA;
-    // Verify URL
-    var verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${req.connection.remoteAddress}`;
-    // Make request to Verify URL
-    request.get(verifyURL, (err, response, body) => {
-      // if not successful
-      if (body.success !== undefined && !body.success) {
-        req.flash("error", "Captcha Failed");
-        return res.redirect("/register");
-      }
+    // // secret key
+    // var secretKey = process.env.CAPTCHA;
+    // // Verify URL
+    // var verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${req.connection.remoteAddress}`;
+    // // Make request to Verify URL
+    // request.get(verifyURL, (err, response, body) => {
+    //   // if not successful
+    //   if (body.success !== undefined && !body.success) {
+    //     req.flash("error", "Captcha Failed");
+    //     return res.redirect("/register");
+    //   }
       var newUser = new User(
       {
         username: req.body.username,

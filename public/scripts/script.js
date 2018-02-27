d6417096026150946278ebf0ed89ef7f50b7f4fc
Ioannis Mer <ioannismeri@gmail.com>
2018-02-27 06:25:47 +0000

    fix thumbnails image sizes

---
+++ public/scripts/script.js
@@ -0,0 +1,43 @@
+$(document).load(function(){
+ $('.card-image').find('img').each(function(){
+  var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+  
+  $(this).addClass(imgClass);
+  
+//   $(this).css("width", this.width * 2);
+//   $(this).css("height", this.width * 2);
+  
+  var width = $(this).width();    // Current image width
+  var height = $(this).height();  // Current image height
+  
+  
+  
+//   if (width > height){
+//       var ratio = width/height;
+//   }
+
+    // // Check if the current width is larger than the max
+    // if(width > maxWidth){
+    //     ratio = maxWidth / width;   // get ratio for scaling image
+    //     $(this).css("width", maxWidth); // Set new width
+    //     $(this).css("height", height * ratio);  // Scale height based on ratio
+    //     height = height * ratio;    // Reset height to match scaled image
+    // }
+
+    // var width = $(this).width();    // Current image width
+    // var height = $(this).height();  // Current image height
+
+    // // Check if current height is larger than max
+    // if(height > maxHeight){
+    //     ratio = maxHeight / height; // get ratio for scaling image
+    //     $(this).css("height", maxHeight);   // Set new height
+    //     $(this).css("width", width * ratio);    // Scale width based on ratio
+    //     width = width * ratio;    // Reset width to match scaled image
+    // }
+  
+  
+  
+  
+  
+ })
+})

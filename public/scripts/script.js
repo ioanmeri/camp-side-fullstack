f6ad09cab8f3a1348aef238c0f9724f3ec49fbd5
Ioannis Mer <ioannismeri@gmail.com>
2018-03-01 04:49:57 +0000

    update forms, clear css

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,6 +6,12 @@ $(document).ready(function(){
     setInterval(function(){
         $("body").removeClass("preload");
     }, 200);
+    
+    window.setTimeout(function() {
+      $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+      });
+    }, 2500);
 
 })
 
